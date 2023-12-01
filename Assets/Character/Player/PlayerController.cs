6e657bce99af5415a9876d852dee6bdac4a92686
Job Ferreira <jobbjunior@hotmail.com>
2023-12-01 11:22:56 -0300

    cahnged the moviment scrip to make more clean the movement when colliding with object(when the player hit an objet it wont stop if he is pressing two keys, he will slide),

--- Assets/Character/Player/PlayerController.cs
+++ Assets/Character/Player/PlayerController.cs
@@ -10,12 +10,11 @@ public class PlayerController : MonoBehaviour
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
 
+    private Vector2 movementInput;
+    private Rigidbody2D rb;
+    private List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
-    Vector2 movementInput;
-    Rigidbody2D rb;
-    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
-
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
@@ -25,24 +24,41 @@ public class PlayerController : MonoBehaviour
     {
         //if movemnt inpput is not 0, try to move
         if (movementInput != Vector2.zero) //movementInput == direction
+        {
+            bool success = TryMove(movementInput); //normal movement
+
+            if (!success)
             {
-            int count = rb.Cast(
-                movementInput, // X and Y values between 1 and -1 that represent the direction form body to look  for collisions
-                movementFilter,//The settings that determine where a collision can occur on such as layer to collide with
-                castCollisions,//List of collisions to store the found collisions into after the Cast is finished
-                moveSpeed * Time.fixedDeltaTime + collisionOffset);// The amount to cast equal to the movement plus an offset ( moveSpeed * Time.fixedDeltaTime this will tell the magnitude of the movement)
-            if (count == 0)
-            {
-                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+                success = TryMove(new Vector2(movementInput.x,0)); // try to move only on X axys
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0,movementInput.y)); // try to move only on T axys
+                }
             }
         }
     }
-    void OnMove(InputValue movementValue)
+
+    private bool TryMove(Vector2 direction)
+    // check for potencial colissions
+    {
+        int count = rb.Cast(
+                 direction, // X and Y values between 1 and -1 that represent the direction form body to look  for collisions
+                 movementFilter,//The settings that determine where a collision can occur on such as layer to collide with
+                 castCollisions,//List of collisions to store the found collisions into after the Cast is finished
+                 moveSpeed * Time.fixedDeltaTime + collisionOffset);// The amount to cast equal to the movement plus an offset ( moveSpeed * Time.fixedDeltaTime this will tell the magnitude of the movement)
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnMove(InputValue movementValue)
     {
         movementInput = movementValue.Get<Vector2>();
     }
-
-
 }
-
-
